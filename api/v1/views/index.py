c6fa09d10910f433f3ca0dbecce19e86e477e534
djamalMhmd <djamaldine-mohamed@live.fr>
2023-08-24 10:48:08 +0200

    Create an endpoint that retrieves the number

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,3 +16,13 @@ from flask import jsonify
 def status():
     """Return status of the server"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def stats():
+    """Return stats of the objects"""
+    classes = {"amenities": Amenity, "cities": City, "places": Place,
+               "reviews": Review, "states": State, "users": User}
+    count_dict = {}
+    for name, cls in classes.items():
+        count_dict[name] = storage.count(cls)
+    return jsonify(count_dict)
